8d58fbed7f94b5e58c98002ccb66898c3a113cde
Alison Shin <alisonyshin@gmail.com>
2018-07-17 07:38:38 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,4 +3,11 @@ def turn_count(board)
   board.each do |cell|
     if cell == "X" or "O"
       counter += 1
+  return counter
+end
+
+def current_player(board)
+  if turn_count(board) %2 == 0 
+    return "O"
+  else return "X"
 end

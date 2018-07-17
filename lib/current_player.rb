9c84bec121c3a0b36d801245b1132a707b65713b
Alison Shin <alisonyshin@gmail.com>
2018-07-17 07:52:31 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,18 +1,8 @@
 board = [" "," "," "," "," ","X"," "," ","O"]
 
-def turn_count(board)
-  counter = 0
-  board.each do |cell|
-    if cell== "X" or cell=="O"
-      counter += 1
-    else
-      counter +=0
-    end
-  return counter
-end
 
 def current_player(board)
-  number_turns = turn_count(board)
+  number_turns = 4
   if number_turns % 2 == 0 
     return "O"
   else 
